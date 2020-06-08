814fd4a3a9e75654778983e22188d7c69d5e3233
Bianca Minnaar <bminnaar@gmail.com>
2020-06-08 15:43:37 +0200

    Implement AddBlog API call - Add API scaffolding for Add Blog in BlogController - Add BlogRepository - Add AddBlog to BlogRepository

--- BlogAPI/Controllers/BlogController.cs
+++ BlogAPI/Controllers/BlogController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogAPI.Repository;
+using DomainModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogAPI.Controllers
@@ -9,6 +11,29 @@ namespace BlogAPI.Controllers
     [Route("api/[controller]")]
     public class BlogController : Controller
     {
-        
+        [HttpPost]
+        public WebAPIResult<Blog> Post([FromBody]Blog value)
+        {
+            try
+            {
+                return new WebAPIResult<Blog>
+                {
+                    Status = new BlogRepository().AddBlog(value)
+                };
+            }
+            catch (Exception excp)
+            {
+                return new WebAPIResult<Blog>
+                {
+                    Status = new ServiceFeedback
+                    {
+                        Message = excp.Message,
+                        StackTrace = excp.StackTrace,
+                        Status = 500
+                    },
+                    DataModel = value
+                };
+            }
+        }
     }
 }
---
+++ BlogAPI/Repository/BlogRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DomainModels;
+
+namespace BlogAPI.Repository
+{
+    public class BlogRepository
+    {
+        public ServiceFeedback AddBlog(Blog model)
+        {
+            return new ServiceFeedback
+            {
+                Message = "Success",
+                Status = 200
+            };
+        }
+    }
+}
