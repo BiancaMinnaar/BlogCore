34773cd195b32540a83c8ae421184e0afa8f7792
Bianca Minnaar <bminnaar@gmail.com>
2020-06-08 15:31:04 +0200

    Add Domain Models - Add Blog - Add BlogEntry - Add User - Add DomainModels Project Referance

---
+++ DomainModels/BlogEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainModels
+{
+    [Table("BlogEntry", Schema = "BlogKata")]
+    public class BlogEntry
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID")]
+        public Guid ID { get; set; }
+        public Guid BlogID { get; set; }
+        public string HeadingText { get; set; }
+        public string EntryText { get; set; }
+        public Guid UserID { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
---
+++ DomainModels/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainModels
+{
+    [Table("User", Schema = "BlogKata")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID")]
+        public Guid ID { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
